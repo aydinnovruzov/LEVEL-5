9066687d780c9af57aa78c6e60300c4a2d7e8aab
Aydın <aydinnovruzov54@gmail.com>
2025-11-22 15:26:41 +0400

    task2

---
+++ task2.cpp
@@ -0,0 +1,13 @@
+// Proqram kvadratin sahesini hesablayir(funksiyadan istifade ederek)
+#include <iostream>
+using namespace std;
+
+void kvadrat() {
+    int a;
+    cin >> a;
+    cout << "Kvadratin sahesi: " << a*a << endl;
+}
+
+int main() {
+    kvadrat();
+}
