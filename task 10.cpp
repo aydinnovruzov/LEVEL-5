422acf504a21a6ed93da63082dd6d7f4ed556e9e
Aydın <aydinnovruzov54@gmail.com>
2025-11-24 09:12:31 +0400

    optimal

--- task 10.cpp
+++ task 10.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 void armstrong() {
-    for(int i = 100; i <= 999; i++) {
-        int a = i/100, b = (i/10)%10, c = i%10;
-        if(a*a*a + b*b*b + c*c*c == i)
+    for (int i = 100; i <= 999; i++) {
+        int a = i / 100;           
+        int b = (i / 10) % 10;     
+        int c = i % 10;            
+
+        int sum = a*a*a + b*b*b + c*c*c;
+
+        if (sum == i)
             cout << i << " ";
     }
     cout << endl;
